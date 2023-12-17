61f81c21fde3c9f2aff84df8f763a436a3c3a93b
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2023-12-17 16:46:28 +0100

    Initial commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Adam Mekush on 17.12.23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+    @State var value = 0.0
+    let tips = [1, 2, 5, 10 , 15, 20]
+    @State var selectedTip = 1
+    
+    @State var selectedPeople = 1
+    
+    @FocusState var isFocused
+    
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Form {
+                    Section {
+                        TextField("Amount", value: $value, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .focused($isFocused)
+                        #if os(macOS)
+                            .padding()
+                        #endif
+                    } header: {
+                        Text("Amount of money")
+                    }
+                    Picker("Tip", selection: $selectedTip) {
+                        ForEach(tips, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.inline)
+                    #if os(macOS)
+                        .padding()
+                    #endif
+                    
+                    Picker("Number of people", selection: $selectedPeople) {
+                        ForEach(0..<101) {
+                            Text("\($0)")
+                        }
+                    }
+                    
+                    Section {
+                        Text(calculateTipPerPerson(value: value, tip: selectedTip, people: selectedPeople), format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        #if os(macOS)
+                            .padding()
+                        #endif
+                    } header: {
+                        Text("Tip per person")
+                    }
+                }
+                .toolbar {
+                    if isFocused {
+                        Button("Done") {
+                            isFocused = false
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Tip calculator")
+        }
+    }
+    
+    private func calculateTipPerPerson(value: Double, tip: Int, people: Int) -> Double {
+        
+        let result: Double = value/100.0*Double(tip)/Double(people)
+        
+        return result
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,19 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Adam Mekush on 17.12.23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TipCalculatorApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
